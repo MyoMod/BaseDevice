916c7e2d188a18a2663a318b375a5b019cb860d6
Leon Farchau <leon@example.com>
2024-07-22 14:33:42 +0200

    Remove all device specific code
    This Repo ought to be a base for all devices, therefore remove all display specific code.

--- source/SampleSink.cpp
+++ source/SampleSink.cpp
@@ -20,9 +20,6 @@
 #include "hardware/i2c.h"
 
 #include "hardware/gpio.h"
-#include "pico_unicorn.hpp"
-#include "display.hpp"
-#include "debug_pins.h"
 
 #include "specificRegisters.h"
 #include "comInterface.h"
@@ -30,25 +27,14 @@
 /************************************************************
  *  DEFINES
  ************************************************************/
-
-const uint32_t BUTTON_A = pimoroni::PicoUnicorn::A;
-const uint32_t BUTTON_B = pimoroni::PicoUnicorn::B;
-const uint32_t BUTTON_X = pimoroni::PicoUnicorn::X;
-const uint32_t BUTTON_Y = pimoroni::PicoUnicorn::Y;
-
-const uint32_t BUTTON_PINS[] = {
-    BUTTON_A,
-    BUTTON_B,
-    BUTTON_X,
-    BUTTON_Y};
-
-/************************************************************
- * Type definitions
- * **********************************************************/
 #define I2C_UNIT i2c1
 #define I2C_ADDR 0x18
 #define SDA_PIN 2
 #define SCL_PIN 3
+
+/************************************************************
+ * Type definitions
+ * **********************************************************/
 
 
 /************************************************************
@@ -86,24 +72,6 @@ void setup()
 {
     stdio_init_all();
 
-    gpio_init(DEBUG_PIN1);
-    gpio_init(DEBUG_PIN2);
-    gpio_init(DEBUG_PIN3);
-    gpio_set_dir(DEBUG_PIN1, GPIO_OUT);
-    gpio_set_dir(DEBUG_PIN2, GPIO_OUT);
-    gpio_set_dir(DEBUG_PIN3, GPIO_OUT);
-    gpio_put(DEBUG_PIN1, 1);
-    gpio_put(DEBUG_PIN2, 1);
-    gpio_put(DEBUG_PIN3, 1);
-
-    for (size_t i = 0; i < 4; i++)
-    {
-        gpio_init(BUTTON_PINS[i]);
-        gpio_set_dir(BUTTON_PINS[i], GPIO_IN);
-    }
-
-    display_init(0.7f);
-
     cominterfaceConfiguration config;
     config.g_i2c = I2C_UNIT;
     config.g_i2cAddr = I2C_ADDR;
@@ -126,20 +94,20 @@ void asyncLoop()
     if (g_sync)
     {
         g_sync = false;
+
+        // Read data in a async way
         static DeviceToHost_t data;
-        for (size_t i = 0; i < 4; i++)
-        {
-            data[i] = !gpio_get(BUTTON_PINS[i]);
-        }
+        data[0] = 0x01;
         comInterfaceSetHIn(&data);
     }
 }
 
 void dataCallback(const HostToDevice_t * const data)
 {
+    // handle new data
     for (size_t bar = 0; bar < data->size(); bar++)
     {
-        display_drawBar(bar, (*data)[bar]);
+        printf("Bar %d: %d\n", bar, data->at(bar));
     }
 }
 
--- source/debug_pins.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define DEBUG_PIN1 1
-#define DEBUG_PIN2 1
-#define DEBUG_PIN3 1
